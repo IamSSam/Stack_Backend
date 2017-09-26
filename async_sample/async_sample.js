974dfc4e61acf84b00933c52cd0268a1e393f9c1
IamSSam <kongse92@gmail.com>
2017-09-26 15:48:45 +0900

    Understand async function

--- async_sample/async_sample.js
+++ async_sample/async_sample.js
@@ -10,24 +10,26 @@ function mysquare(number, callback) {
 
 var data = [1,2,3,4,5,6,7,8,9,10];
 
-async.map(data, mysquare, function(err, result) { 
+async.map(data, mysquare, function(err, result) {
     console.log('ASYNC.MAP()', result);
   });
 
 async.filter(data, function(item, callback) {
     callback( item % 2 == 0); // we must call the callback given from the inside of 'async'
   },
-  function(results) { 
+  function(results) {
     console.log('ASYNC.FILTER()', results);
   });
 
 
 async.series([
     function(callback){
-      callback(null, 'res1');
+      console.log('Doing DB');
+      callback('DB error', 'res1');
     },
     function(callback){
-      callback(null, 'res2');
+      console.log('Doing Network');
+      callback('network error', 'res2');
     },
     function(callback){
       callback(null, 'res3');
@@ -35,57 +37,60 @@ async.series([
   ],
   function(err, results){
     console.log('ASYNC.SERIES() SUCC', err, results);
-  });
+   });
 
-async.series([
-    function(callback){
-      callback(null, 'res1');
-    },
-    function(callback){
-      var err=true;
-      if (err) 
-        callback('error2 happened', null);
-      else
-        callback(null, 'res2');
-    },
-    function(callback){
-      callback(null, 'res3');
-    }
-  ],
-  function(err, results){
-    console.log('ASYNC.SERIES() FAIL', err, results);
-  });
-
-
+// async.series([
+//     function(callback){
+//       callback(null, 'res1');
+//     },
+//     function(callback){
+//       var err=true;
+//       if (err)
+//         callback('error2 happened', null);
+//       else
+//         callback(null, 'res2');
+//     },
+//     function(callback){
+//       callback(null, 'res3');
+//     }
+//   ],
+//   function(err, results){
+//     console.log('ASYNC.SERIES() FAIL', err, results);
+//   });
+//
+//
 async.waterfall([
     function(cb){
       cb(null, 'res1');
     },
     function(arg1, callback){
-      callback(null, 'res2', 'res3');
+      callback('some error', 'res2', 'res3');
     },
     function(arg1, arg2, callback){
       callback(null, 'finished');
     }
   ],
   function(err, results){
-    console.log('ASYNC.WATERFALL() SUCC', err, results);
+    console.log('ASYNC.WATERFALL() ', err, results);
   });
 
-async.waterfall([
-    function(cb){
-      
-      cb(null, 'res1');
-    },
-    function(arg1, callback){
-        
-      callback('error in second', 'res2', 'res3');
-    },
-    function(arg1, arg2, callback){
-      callback(null, 'finished');
-    }
-  ],
-  function(err, result, result2){
-    console.log('ASYNC.WATERFALL() FAIL', err, result, result2);
-  });
+// series 함수와 waterfall 함수를 잘 쓰면 좋다.
 
+//
+// async.waterfall([
+//     function(cb){
+//
+//       cb(null, 'res1');
+//     },
+//     function(arg1, callback){
+//
+//       callback('error in second', 'res2', 'res3');
+//     },
+//     function(arg1, arg2, callback){
+//       callback(null, 'finished');
+//     }
+//   ],
+//   function(err, result, result2){
+//     console.log('ASYNC.WATERFALL() FAIL', err, result, result2);
+//   });
+//
